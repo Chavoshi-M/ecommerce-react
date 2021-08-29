16e85146f331ba2471dfc64b2e3c2b2af2e15657
Chavoshi-M <chavoshimojtaba92@gmail.com>
2021-08-29 19:26:52 +0330

    broken home page into smaller component,finished syling menu item component

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,25 @@
+import './MenuItem.scss'
+const MenuItem = ({title,imageUrl,size}) => {
+	console.log(title)
+	return (
+		<div className={`menu-item ${size}`}
+		>
+			<div 
+				className="background-image"
+				style={{
+					backgroundImage:`url(${imageUrl})`
+				}}
+			/>
+			<div className="content">
+				<h1 className="title">
+					{title.toUpperCase()}
+				</h1>
+				<span className="subtitle">
+					shop now
+				</span>
+			</div>
+		</div>
+	);
+}
+ 
+export default MenuItem;
---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import './homepage.scss';
+import Directory from '../../components/Directory/Directory'
+const Homepage = () => {
+	return (
+		<div className="homepage">
+			<Directory/>
+		</div>
+	);
+}
+ 
+export default Homepage;
