25e864f4f40b2d1ef5b405589817b315df4ac43c
Chavoshi-M <chavoshimojtaba92@gmail.com>
2021-09-03 17:08:09 +0330

    create directory redux reducer,add directory state into redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,21 @@
 import {combineReducers} from 'redux'
 import userReducer from './user/reducer';
 import cartReducer from './cart/reducer';
+import directoryReducer from './Directory/reducer';
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+
+const persistConfig = {
+	key:'root',
+	storage,
+	whitelist:[
+		'cart'
+	]
+}
+const rootReducer = combineReducers({
 	cart:cartReducer,
+	directory:directoryReducer,
 	user:userReducer
 })
+export default persistReducer(persistConfig,rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import  {createStore,applyMiddleware,compose } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
-
+import { persistStore } from 'redux-persist';
 const middleware = [logger];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
-
-export default store;
+const persistor = persistStore(store);
+export {store,persistor};
